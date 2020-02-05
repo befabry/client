a26c64398cbd4b94b515e0e98890d31db5e31221
befabry <17452648+befabry@users.noreply.github.com>
2020-02-05 15:19:30 +0100

    Update object.js
    Add examples

--- src/helpers/object.js
+++ src/helpers/object.js
@@ -2,9 +2,18 @@
  * Takes an array or an object as parameter and return an object with the given func as key and the same value
  * Inspired by Lodash and by : https://www.30secondsofcode.org/js/s/map-keys
  *
+ * Examples :
+ * let obj = {0:{"id": 5, "test": "test", "test2": "test2"}, 1:{"id": 10, "test": "test", "test2": "test2"}};
+ * mapKeys(obj, val => val.id);
+ * output : { 5: Object { id: 5, test: "test", test2: "test2" }, 10: { id: 10, test: "test", test2: "test2" }}
+ *
+ * let arr = [{"id": 5, "test": "test", "test2": "test2"}, {"id": 10, "test": "test", "test2": "test2"}];
+ * mapKeys(arr, val => val.id);
+ * output : {5: { id: 5, test: "test", test2: "test2" }, 10: { id: 10, test: "test", test2: "test2" }}
+ * 
  * @param array
- * @param func same arguments as forEach can be passed
- * @returns {{}}
+ * @param func
+ * @returns {{}} 
  */
 const mapKeys = (obj, func) => {
     if(typeof obj !== "object"){
